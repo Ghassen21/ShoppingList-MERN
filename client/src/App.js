efc3249b9215467f8e6dfa86bd4ca7bf978ed719
Ghassen21 <ghassenayd@gmailcom>
2021-03-25 20:39:14 +0100

    create the item list and render it to the  Ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import AppNav from "./components/AppNav"
+import ShoppingList from "./components/ShoppingList"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 function App() {
   return (
     <div className="App">
      <AppNav/>
+     <ShoppingList/>
     </div>
   );
 }
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { Button } from 'reactstrap';
-import uuid from 'uuid';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import {v1 as uuid} from "uuid";
 export default class ShoppingList extends Component {
     constructor(props) {
         super(props);
-      this.state = {
-            items: [{id:uuid(),name:"Eggs"},{id:uuid(),name:"Milk"},{id:uuid(),name:"Steak"},{id:uuid(),name:"Water"}]
+        this.state = {
+            items: [{ id: uuid(), name: "Eggs" }, { id: uuid(), name: "Milk" }, { id: uuid(), name: "Steak" }, { id: uuid(), name: "Water" }]
         }
     }
     render() {
-        const {items}=this.state
+        const { items } = this.state
+        console.log("items: ",items)
         return (
-           <Container>
-               <Button></Button>
-           </Container>
+            <Container>
+                <Button color="dark" style={{ marginBottom: "2rem" }}
+                    onClick={()=> { const name = prompt('Enter Item') ;
+            if (name){
+                    this.setState(state => ({
+                        items: [...state.items, { id: uuid(), name :name}]
+                    }))
+                }}}>Add Item</Button>
+                <ListGroup>
+        <TransitionGroup className="shopping-list">
+          {items.map(( item) => (
+            <CSSTransition key={item.id} timeout={500} classNames="fade">
+              <ListGroupItem>
+               {item.name }
+              </ListGroupItem>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </ListGroup>
+           </Container >
         )
     }
 }
