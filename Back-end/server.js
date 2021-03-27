74a8c6772c9c222ffc78a679ed20e548d03cb0b0
Ghassen21 <ghassenayd@gmailcom>
2021-03-27 04:08:19 +0100

    ready to cmmit

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+ const path =require ('path');
 require("dotenv").config();
 const app = express();
 
@@ -46,7 +46,14 @@ app.use('/api/items', itemRoutes);
 //app.use('/api/users', userRoutes);
 //app.use('/api/auth', authRoutes);
 
+// Serve static assets if in porduction
+if (process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'))
 
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
+  })
+}
 const PORT = process.env.PORT || 5000;
 
 
